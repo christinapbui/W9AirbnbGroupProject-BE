7ad167eb3d533e79b957fd58fa297812f0d54b0e
Minh Nguyen <gin411@Minhs-MacBook-Pro-2.local>
2020-07-03 05:23:10 -0400

    Finalizing pagination for BE

--- controllers/experiencesController.js
+++ controllers/experiencesController.js
@@ -1,41 +1,31 @@
 const Experience = require("../models/experience");
 
 const getAllExperiences = async (req, res) => {
-  const page = parseInt(req.query.page);
-  const limit = parseInt(req.query.limit);
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
 
-  const startIndex = (page -1) * limit;
-  const endIndex = page*limit;
-  const experiences = await Experience.find();
-  const results ={};
-
-  if(endIndex<experiences.length){
-    results.next ={
-      page: page +1,
-      limit: limit
-    }
-  }else{
-    return res.status(400).json({message: "Page number out of range"})
-  }
-
+  const skip = (page -1) * limit;
+  // const endIndex = page*limit;
+  let query =  Experience.find();
   
-  if(startIndex>0){
-    results.previous ={
-      page: page -1,
-      limit: limit
-    }
-  }else{
-    return res.status(400).json({message: "Page number out of range"})
-  }
+
+
+  query = query.skip(skip).limit(limit);
   
 
   
-    results.results = experiences.slice(startIndex,endIndex)
-    
+  const countExperiences =  await Experience.find( ).countDocuments() // total docs
+    if (page && skip > countExperiences)
+      return res.send("Out of range")
 
+    // execute
+
+    // res.json({ status: "success", data: exps, count: countExperiences }); // add countExperiences as well
+
+    const experiences = await query
   
-  res.send(results);
-
+  // res.send(results);
+res.send({ data: experiences, total: countExperiences, page: page, perpage: limit})
   
 };
 
@@ -50,7 +40,7 @@ const createExperience = async (req, res) => {
   const language = req.body.language
   const description = req.body.description
   const host = req.body.host
-  const whatToBring = eq.body.whatToBring
+  const whatToBring = req.body.whatToBring
   console.log(req.body)
   const newExperience = await Experience.create({
     title,
--- routes/experiences.js
+++ routes/experiences.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router({mergeParams:true});
 // const Experience = require("../models/experience"); // need to import model
 const { getAllExperiences, createExperience, getSingleExperience, updateExperience } = require("../controllers/experiencesController");
-// const detailsController = require("../controllers/detailsController");
+// const detailsController = require("../controllers/experiencesController");
 // var detailRouter = require("../detail")
 
 // const Experience = require('../models/experience')
