a77e2d6d140974ef31b3785c2be43fb07b125b0a
Minh Nguyen <gin411@Minhs-MacBook-Pro-2.local>
2020-07-03 02:05:02 -0400

    finishing pagination in backend side

--- routes/experiences.js
+++ routes/experiences.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router({mergeParams:true});
 // const Experience = require("../models/experience"); // need to import model
 const { getAllExperiences, createExperience, getSingleExperience, updateExperience } = require("../controllers/experiencesController");
-const detailsController = require("../controllers/detailsController");
-var detailRouter = require("../detail")
+// const detailsController = require("../controllers/detailsController");
+// var detailRouter = require("../detail")
 
 // const Experience = require('../models/experience')
 
